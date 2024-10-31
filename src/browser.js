d9290363d1d0f4dafd514fbb376fcf629be53398
Kim Lai <a0979470582@gmail.com>
2024-10-31 10:48:52 +0800

    first commit

---
+++ src/browser.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = require("./index");
+module.exports.axios = require("axios");
+module.exports.bluebird = require("bluebird");
+module.exports.StellarBase = require("js-kinesis-base");
+
+/*globals _*/
+_.noConflict();
